217bae7b5db6417d1e62cd89f05f2ab3dcc166f2
GiraGiT <65813708+GiraGiT@users.noreply.github.com>
2024-05-19 21:20:15 +0500

    Add files via upload

---
+++ test.lua
@@ -0,0 +1,43 @@
+-- Poluchaem dostup k periferiyam
+local chest = peripheral.wrap("minecraft:chest_1")
+local drawer = peripheral.wrap("storagedrawers:standard_drawers_1_1")
+local monitor = peripheral.wrap("top")
+
+-- Poluchaem maksimalnuyu vmestimost sunduka
+local total1 = 0
+local total2 = 0
+
+for i = 1, chest.size() do
+  total1 = total1 + chest.getItemLimit(i)
+end
+
+for i = 1, drawer.size() do
+  total2 = total2 + drawer.getItemLimit(i)
+end
+
+-- Poluchaem tekuschee kolichestvo predmetov
+local current1 = 0
+local current2 = 0
+
+for i = 1, chest.size() do
+  current1 = current1 + chest.getItemCount(i)
+end
+
+for i = 1, drawer.size() do
+  current2 = current2 + drawer.getItemCount(i)
+end
+
+-- Vivodim informatsiyu na monitor
+monitor.clear()
+monitor.setCursorPos(1, 1)
+monitor.write("Maksimalnaya vmestimost sunduka: " .. total1)
+monitor.setCursorPos(1, 2)
+monitor.write("Tekuschee kolichestvo predmetov v sunduke: " .. current1)
+monitor.setCursorPos(1, 3)
+monitor.write("Sunduk zapolnen na: " .. (current1 / total1) * 100 .. "%")
+monitor.setCursorPos(1, 5)
+monitor.write("Maksimalnaya vmestimost yashchika: " .. total2)
+monitor.setCursorPos(1, 6)
+monitor.write("Tekuschee kolichestvo predmetov v yashchike: " .. current2)
+monitor.setCursorPos(1, 7)
+monitor.write("Yashchik zapolnen na: " .. (current2 / total2) * 100 .. "%")
