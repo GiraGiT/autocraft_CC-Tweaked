9c43cccfc40d89d847971ee5d8d6f9d13f6cdb2c
GiraGiT <65813708+GiraGiT@users.noreply.github.com>
2024-05-23 20:40:52 +0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,50 +1,37 @@
 while true do
   -- Getting access to peripherals
-  local chest = peripheral.wrap("minecraft:chest_2")
-  local drawer = peripheral.wrap("storagedrawers:standard_drawers_1_2")
+  local peripherals = peripheral.getNames()
   local monitor = peripheral.wrap("top")
+  
+  for _, name in ipairs(peripherals) do
+    local storage = peripheral.wrap(name)
+    
+    -- Check if the peripheral has the necessary functions
+    if storage.getItemDetail and storage.getItemLimit then
+      -- Getting the maximum capacity of the storage
+      local total = 0
+      for i = 1, storage.size() do
+        total = total + storage.getItemLimit(i)
+      end
 
-  -- Getting the maximum capacity of the chest
-  local total1 = 0
-  local total2 = 0
+      -- Getting the current number of items
+      local current = 0
+      for i = 1, storage.size() do
+        local item = storage.getItemDetail(i)
+        if item then
+          current = current + item.count
+        end
+      end
 
-  for i = 1, chest.size() do
-    total1 = total1 + chest.getItemLimit(i)
-  end
-
-  for i = 1, drawer.size() do
-    total2 = total2 + drawer.getItemLimit(i) -- Исправлено здесь
-  end
-
-  -- Getting the current number of items
-  local current1 = 0
-  local current2 = 0
-
-  for i = 1, chest.size() do
-    local item = chest.getItemDetail(i)
-    if item then
-      current1 = current1 + item.count
+      -- Displaying information on the monitor
+      monitor.setTextScale(0.5)
+      monitor.clear()
+      monitor.setCursorPos(1, 1)
+      monitor.write("Maximum " .. name .. " capacity: " .. total)
+      monitor.setCursorPos(1, 2)
+      monitor.write("Current number of items in the " .. name .. ": " .. current)
     end
   end
-
-  for i = 1, drawer.size() do
-    local item = drawer.getItemDetail(i)
-    if item then
-      current2 = current2 + item.count
-    end
-  end
-
-  -- Displaying information on the monitor
-  monitor.setTextScale(0.5)
-  monitor.clear()
-  monitor.setCursorPos(1, 1)
-  monitor.write("Maximum chest capacity: " .. total1)
-  monitor.setCursorPos(1, 2)
-  monitor.write("Current number of items in the chest: " .. current1)
-  monitor.setCursorPos(1, 3)
-  monitor.write("Maximum drawer capacity: " .. total2)
-  monitor.setCursorPos(1, 4)
-  monitor.write("Current number of items in the drawer: " .. current2)
 
   -- Sleep for 1 second
   os.sleep(1)
